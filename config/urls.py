3fbe6cc385216c5ce1ee2a0bb6803897475f9cc5
nagi <ymxlinkinparkxlc@gmail.com>
2020-07-03 17:43:09 +0900

    Update templates and Fix error

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -8,3 +9,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('apiv1.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
